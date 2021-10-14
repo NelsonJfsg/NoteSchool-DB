f98dc696fc14a3d863e73410061dea16973fc406
NelsonJfsg <NelsonJfsg@DESKTOP-9KVSAP9>
2021-10-14 14:19:59 -0700

    Agregar archivos de proyecto.

---
+++ NoteSchool/Main/Start.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NoteSchool.Layout.Load;
+
+namespace NoteSchool.Main
+{
+    class Start
+    {
+
+        public static void Main()
+        {
+
+            //ejecutar load.
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new ScSplash());
+
+        }
+    }
+}
